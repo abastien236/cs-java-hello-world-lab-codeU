dfd7c92a4d24d47f66865e492873a85a48b68433
Anwaar Bastien <Anwaar@abastien.student.umd.edu>
2016-04-16 16:36:32 -0400

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
     }
 
     public static void main(String[] args) {
-	    System.out.println("hello");
+      Double vers = getVersion();
+	    System.out.println(vers);
     }
 }
